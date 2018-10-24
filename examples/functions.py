eceae64eb5e6b67a299f8a54969c68d8c1a95ff4
Joel Eager <jee7291@rit.edu>
2018-10-24 00:07:34 -0400

    Implemented the functions demos

--- examples/functions.py
+++ examples/functions.py
@@ -1,7 +1,27 @@
 """
-First-class functions in Python and some of their uses.
+First-class functions in Python.
 
 More information:
     https://en.wikipedia.org/wiki/First-class_function
     https://developer.mozilla.org/en-US/docs/Glossary/First-class_Function
 """
+
+
+def increment(value):
+    return value + 1
+
+
+def decrement(value):
+    return value - 1
+
+
+def apply_function(funct, value, iterations):
+    for index in range(iterations):
+        value = funct(value)
+
+    return value
+
+
+if __name__ == "__main__":
+    print("Increment:", apply_function(increment, 0, 10))
+    print("Decrement:", apply_function(decrement, 0, 10))
