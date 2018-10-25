2bc5b7bff6a19def2595a3969a4ba3bb88f56e45
Joel Eager <jee7291@rit.edu>
2018-10-24 22:19:41 -0400

    Improved docs

--- code/generators.py
+++ code/generators.py
@@ -3,4 +3,5 @@ Iterator creation using Python generator syntax.
 
 Docs:
     https://docs.python.org/3/howto/functional.html#generators
+    https://docs.python.org/3/tutorial/classes.html#generator-expressions
 """
