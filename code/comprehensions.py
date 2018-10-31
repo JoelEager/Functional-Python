12380591a9a59c7c6926f97c10f0cb7f184b99c4
Joel Eager <jee7291@rit.edu>
2018-10-30 22:12:41 -0400

    Added another example and improved comments for comprehensions.py

--- code/comprehensions.py
+++ code/comprehensions.py
@@ -1,5 +1,6 @@
 """
 An alternate use of iterators to populate Python data structures in a compact and readable way.
+    (Comprehensions are basically syntactical sugar for map(), filter(), and constructors.)
 
 Docs:
     List comprehensions: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
@@ -20,7 +21,10 @@ def run_demo():
     print(a_list, a_set, a_dict, sep="\n")
 
     print("\nArbitrary expressions can go in the first part of the comprehension")
-    print([randint(index, index ** 2) for index in range(10)])
+    print([index ** 2 for index in range(10)])
+
+    print("\nYou can also add an 'if' clause to the end to filter the items")
+    print([value for value in range(10) if value % 2 == 0])
 
     print("\nIf the value returned from the iterator isn't used in your expression you can use a _ to discard it")
     print([randint(0, 100) for _ in range(10)])
